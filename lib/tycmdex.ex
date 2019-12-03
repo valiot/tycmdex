67d113b2d62ffd0d3045b74df1d59d46f5f9bc2b
alde103 <aldebaran_103@hotmail.com>
2019-12-02 19:10:36 -0600

    initial commit

---
+++ lib/tycmdex.ex
@@ -0,0 +1,18 @@
+defmodule Tycmdex do
+  @moduledoc """
+  Documentation for Tycmdex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tycmdex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tycmdex_test.exs
@@ -0,0 +1,8 @@
+defmodule TycmdexTest do
+  use ExUnit.Case
+  doctest Tycmdex
+
+  test "greets the world" do
+    assert Tycmdex.hello() == :world
+  end
+end
